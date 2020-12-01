73a84b36779c5eda0964d6c071c57a7228337d5d
nikhil627050 <73420835+nikhil627050@users.noreply.github.com>
2020-12-01 10:11:39 +0530

    Add files via upload

---
+++ SupplyMission-master (2)/SupplyMission-master/package.js
@@ -0,0 +1,25 @@
+class box{
+    constructor(x,y,width,height){
+        var options={
+            restitution:2,
+            friction:0.2,
+            density:1
+                         }
+    
+    
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+       World.add(world,this.body);
+    }
+    
+    display(){
+        var pos=this.body.position
+        rectMode(CENTER);
+        fill(255)
+        rect(pos.x,pos.y,this.width,this.height)
+    
+    
+    
+    }
+    }
